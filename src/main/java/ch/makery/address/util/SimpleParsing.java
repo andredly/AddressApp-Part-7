29f9c8b3ffb4df72b0c1a92bd0c1fdb61d871242
GRODNO\Andrei_Charniauski <andrei_charniauski@epam.com>
2018-03-02 18:22:35 +0300

    maven edit

--- src/main/java/ch/makery/address/util/SimpleParsing.java
+++ src/main/java/ch/makery/address/util/SimpleParsing.java
@@ -1,5 +1,6 @@
 package ch.makery.address.util;
 
+import javafx.collections.ObservableList;
 import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -9,55 +10,63 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public class SimpleParsing {
-     public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-         File fXmlFile = new File("D:/Install/fx/1/AddressApp-Part-7/poms.xml");
+        File fXmlFile = new File("poms.xml");
 
-         try {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+            List<String> list = new ArrayList<>();
+            NodeList nList = doc.getElementsByTagName("modules");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node node = nList.item(temp);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    list.add(eElement.getTextContent());
+                }
+            }
+            System.out.println(list);
+            if (list.contains("bundles")){
+                fXmlFile = new File("bundles/"+ "poms.xml");
+            }
+            doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+            list.clear();
+            nList = doc.getElementsByTagName("bundles");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node node = nList.item(temp);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    list.add(eElement.getTextContent());
+                }
+            }
+            System.out.println(list);
 
 
-             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-             Document doc = dBuilder.parse(fXmlFile);
-             doc.getDocumentElement().normalize();
-             NodeList nList = doc.getElementsByTagName("modules");
+//             File root = new File("c:\\");
+//
+//             String[] extensions = { "xml", "java", "dat" };
+//             boolean recursive = true;
+//
+//             Collection files = FileUtils.listFiles(root, extensions, recursive);
+//
+//             for (Iterator iterator = files.iterator(); iterator.hasNext();) {
+//                 File file = (File) iterator.next();
+//                 System.out.println("File = " + file.getAbsolutePath());
+//             }
 
-
-             File root = new File("c:\\");
-
-             String[] extensions = { "xml", "java", "dat" };
-             boolean recursive = true;
-
-             Collection files = FileUtils.listFiles(root, extensions, recursive);
-
-             for (Iterator iterator = files.iterator(); iterator.hasNext();) {
-                 File file = (File) iterator.next();
-                 System.out.println("File = " + file.getAbsolutePath());
-             }
-
-
-             for (int temp = 0; temp < nList.getLength(); temp++) {
-
-                 Node nNode = nList.item(temp);
-
-                 System.out.println("\nCurrent Element :" + nNode.getNodeName());
-
-                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-                     Element eElement = (Element) nNode;
-
-                     System.out.println(eElement.getTextContent());
-
-                 }
-             }
-         } catch (Exception e) {
-             e.printStackTrace();
-         }
-     }
-     }
-//         }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
