8ed30758adf24359899989763133aa8cf346b3b1
GRODNO\Andrei_Charniauski <andrei_charniauski@epam.com>
2018-03-21 16:44:39 +0300

    install

--- src/main/java/ch/makery/address/util/NetUtil.java
+++ src/main/java/ch/makery/address/util/NetUtil.java
@@ -1,39 +1,27 @@
 package ch.makery.address.util;
 
-import com.sun.jndi.toolkit.url.Uri;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.entity.mime.*;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.*;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.SystemDefaultCredentialsProvider;
 import org.apache.http.util.EntityUtils;
-
-import static org.toilelibre.libe.curl.Curl.curl;
-import static org.toilelibre.libe.curl.Curl.$;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class NetUtil {
 
@@ -62,21 +50,19 @@ public class NetUtil {
     public NetUtil() throws IOException {
     }
 
-    public static void main(String[] args) throws IOException, AuthenticationException {
-        new NetUtil().get();
-
+    public static void installPackage(String localhost, int port, String login, String password, String filePath) throws IOException, AuthenticationException {
         HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
         SystemDefaultCredentialsProvider systemDefaultCredentialsProvider = new SystemDefaultCredentialsProvider();
-        HttpHost targetHost = new HttpHost("localhost", 4702, "http");
+        HttpHost targetHost = new HttpHost(localhost, port, "http");
         systemDefaultCredentialsProvider.setCredentials(new AuthScope(targetHost.getHostName(), targetHost.getPort(), AuthScope.ANY_REALM),
-                new UsernamePasswordCredentials("admin", "admin"));
+                new UsernamePasswordCredentials(login, password));
         httpClientBuilder.setDefaultCredentialsProvider(systemDefaultCredentialsProvider);
         HttpPost httpPost = new HttpPost(targetHost.getSchemeName() + "://" + targetHost.getHostName() + ":" + targetHost.getPort() + "/crx/packmgr/service.jsp");
         MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
         multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
         multipartEntityBuilder.addPart("force", new StringBody("true", ContentType.TEXT_PLAIN));
         multipartEntityBuilder.addPart("install", new StringBody("true", ContentType.TEXT_PLAIN));
-        multipartEntityBuilder.addPart("file", new FileBody(new File("d:/Install/fx/test1.zip")));
+        multipartEntityBuilder.addPart("file", new FileBody(new File(filePath)));
         httpPost.setEntity(multipartEntityBuilder.build());
         System.out.println("executing request " + httpPost.getRequestLine());
         HttpResponse httpResponse = httpClientBuilder.build().execute(httpPost);
@@ -86,5 +72,4 @@ public class NetUtil {
             System.out.println(EntityUtils.toString(entity));
         }
     }
-//    }
 }
