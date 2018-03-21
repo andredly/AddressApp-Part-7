1c5d6a6aeb12b3682e1033e08779459dfa29ce86
GRODNO\Andrei_Charniauski <andrei_charniauski@epam.com>
2018-03-21 16:37:46 +0300

    install

--- src/main/java/ch/makery/address/util/NetUtil.java
+++ src/main/java/ch/makery/address/util/NetUtil.java
@@ -1,18 +1,39 @@
 package ch.makery.address.util;
 
+import com.sun.jndi.toolkit.url.Uri;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.*;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.*;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 
+import static org.toilelibre.libe.curl.Curl.curl;
+import static org.toilelibre.libe.curl.Curl.$;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NetUtil {
 
@@ -29,8 +50,7 @@ public class NetUtil {
             response1.getAllHeaders();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally
-        {
+        } finally {
             try {
                 response1.close();
             } catch (IOException e) {
@@ -45,35 +65,26 @@ public class NetUtil {
     public static void main(String[] args) throws IOException, AuthenticationException {
         new NetUtil().get();
 
-//        CloseableHttpClient client = HttpClients.createDefault();
-//        HttpPost httpPost = new HttpPost("http://www.example.com");
-//
-//        String json = "{\"id\":ff,\"name\":\"John\"}";
-//        StringEntity entity = new StringEntity(json);
-//        httpPost.setEntity(entity);
-//        httpPost.setHeader("Accept", "application/json");
-//        httpPost.setHeader("Content-type", "application/json");
-//
-//        CloseableHttpResponse response = client.execute(httpPost);
-//        HttpEntity entity1 = response.getEntity();
-//        String theString = IOUtils.toString(response.getEntity().getContent(), "UTF-8");
-//        response.getStatusLine().getStatusCode();
-//        client.close();
-
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://www.example.com");
-
-        httpPost.setEntity(new StringEntity("test post"));
-        UsernamePasswordCredentials creds
-                = new UsernamePasswordCredentials("John", "pass");
-        httpPost.addHeader(new BasicScheme().authenticate(creds, httpPost, null));
-
-        CloseableHttpResponse response = client.execute(httpPost);
-        response.getStatusLine().getStatusCode();
-        client.close();
-
-//        http://www.baeldung.com/httpclient-post-http-request
-//        https://docs.postman-echo.com/
-//        https://superuser.com/questions/149329/what-is-the-curl-command-line-syntax-to-do-a-post-request
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        SystemDefaultCredentialsProvider systemDefaultCredentialsProvider = new SystemDefaultCredentialsProvider();
+        HttpHost targetHost = new HttpHost("localhost", 4702, "http");
+        systemDefaultCredentialsProvider.setCredentials(new AuthScope(targetHost.getHostName(), targetHost.getPort(), AuthScope.ANY_REALM),
+                new UsernamePasswordCredentials("admin", "admin"));
+        httpClientBuilder.setDefaultCredentialsProvider(systemDefaultCredentialsProvider);
+        HttpPost httpPost = new HttpPost(targetHost.getSchemeName() + "://" + targetHost.getHostName() + ":" + targetHost.getPort() + "/crx/packmgr/service.jsp");
+        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+        multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+        multipartEntityBuilder.addPart("force", new StringBody("true", ContentType.TEXT_PLAIN));
+        multipartEntityBuilder.addPart("install", new StringBody("true", ContentType.TEXT_PLAIN));
+        multipartEntityBuilder.addPart("file", new FileBody(new File("d:/Install/fx/test1.zip")));
+        httpPost.setEntity(multipartEntityBuilder.build());
+        System.out.println("executing request " + httpPost.getRequestLine());
+        HttpResponse httpResponse = httpClientBuilder.build().execute(httpPost);
+        HttpEntity entity = httpResponse.getEntity();
+        System.out.println(httpResponse.getStatusLine());
+        if (entity != null) {
+            System.out.println(EntityUtils.toString(entity));
+        }
     }
+//    }
 }
